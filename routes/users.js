fd0d7606855c2ee23402084c39587c988b2ba4cc
Adrian Rogers <adritek@gmail.com>
2024-04-01 19:33:01 -0400

    update GET ops for all and email

--- routes/users.js
+++ routes/users.js
@@ -24,20 +24,20 @@ let users = [
 ];
 
 // GET request: Retrieve all users
-router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.get("/", (req, res) => {
+    res.send(users);
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
-router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.get("/:email", (req, res) => {
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    res.send(filtered_users);
 });
 
 
 // POST request: Create a new user
-router.post("/",(req,res)=>{
+router.post("/", (req, res) => {
   // Copy the code here
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
