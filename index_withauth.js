f0fa57c75464d2fb76df86630caf2fdd4eea5881
Adrian Rogers <adritek@gmail.com>
2024-04-02 19:20:33 -0400

    formatting

--- index_withauth.js
+++ index_withauth.js
@@ -4,26 +4,25 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 
 const app = express();
-const PORT =5000;
+const PORT = 5000;
 
-app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}))
+app.use(session({secret: "fingerpint", resave: true, saveUninitialized: true}))
 
 app.use(express.json());
 
-app.use("/user", (req,res,next)=>{
+app.use("/user", (req, res, next) => {
 // Middleware which tells that the user is authenticated or not
    
-   if(req.session.authorization) {
-       let token = req.session.authorization['accessToken']; // Access Token
+    if(req.session.authorization) {
+        let token = req.session.authorization['accessToken']; // Access Token
        
-       jwt.verify(token, "access",(err,user)=>{
-           if(!err){
-               req.user = user;
-               next();
-           }
-           else{
-               return res.status(403).json({message: "User not authenticated"})
-           }
+        jwt.verify(token, "access", (err, user) => {
+            if(!err){
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({message: "User not authenticated"})
+            }
         });
     } else {
         return res.status(403).json({message: "User not logged in"})
@@ -32,19 +31,23 @@ app.use("/user", (req,res,next)=>{
 
 app.use("/user", routes);
 
-app.post("/login", (req,res) => {
+app.post("/login", (req, res) => {
     const user = req.body.user;
+    
     if (!user) {
         return res.status(404).json({message: "Body Empty"});
     }
+
     let accessToken = jwt.sign({
         data: user
-      }, 'access', { expiresIn: 60 * 60 });
+        }, 
+        'access', 
+        { expiresIn: 60 * 60 }
+    );
 
-      req.session.authorization = {
-        accessToken
-    }
+    req.session.authorization = {accessToken}
+
     return res.status(200).send("User successfully logged in");
 });
 
-app.listen(PORT,()=>console.log("Server is running at port "+PORT));
+app.listen(PORT, ()=>console.log(`Server is running at port: ${PORT}`));
